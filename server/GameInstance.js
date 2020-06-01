833bac1c74e9cf7b172ecb2b7d9576c78d8a208a
Alex <alex@timetocode.com>
2020-06-01 04:44:05 -0700

    fixed player meshes staying in-game after disconnecting

--- server/GameInstance.js
+++ server/GameInstance.js
@@ -74,6 +74,8 @@ class GameInstance {
 
 		this.instance.on('disconnect', client => {
 			// clean up per client state
+			client.rawEntity.mesh.dispose()
+			client.smoothEntity.mesh.dispose()
 			this.instance.removeEntity(client.rawEntity)
 			this.instance.removeEntity(client.smoothEntity)
 			client.channel.destroy()
