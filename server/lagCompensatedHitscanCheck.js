9d42434f4eb41a2929252dd5f5b0fc60dc3361f4
Filip Przychodni <pierwiastekz3@interia.pl>
2021-01-25 19:00:43 +0100

    Added z and halfDepth to area

--- server/lagCompensatedHitscanCheck.js
+++ server/lagCompensatedHitscanCheck.js
@@ -1,7 +1,7 @@
 export default (instance, ray, timeAgo) => {
 	// note: there is no 3D lag compensation culler in nengi at the moment
 	// so we just create an area that includes the whole game
-	const area = { x: 0, y: 0, halfWidth: 999999, halfHeight: 999999 }
+	const area = { x: 0, y: 0, z: 0, halfWidth: 999999, halfHeight: 999999, halfDepth: 999999}
 
 	const hits = []
 	const pastEntities = instance.historian.getLagCompensatedArea(timeAgo, area)
